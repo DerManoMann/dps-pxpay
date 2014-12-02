ebacfee9ab761647dae20e2b4d66ab2d8aa9430f
Larry Bislew <bislewl@gmail.com>
2014-12-02 17:14:21 -0600

    fix curl

--- zen-cart/includes/modules/payment/dps_pxpay.php
+++ zen-cart/includes/modules/payment/dps_pxpay.php
@@ -582,26 +582,20 @@ class dps_pxpay {
 
         curl_setopt($curl, CURLOPT_URL, $this->_dpsPxpayUrl);
         curl_setopt($curl, CURLOPT_FRESH_CONNECT, true);
-        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
         if (defined('CURLOPT_POSTFIELDSIZE')) {
             curl_setopt($curl, CURLOPT_POSTFIELDSIZE, 0);
         }
         curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
         curl_setopt($curl, CURLOPT_HEADER, 0);
- //       curl_setopt($curl, CURLOPT_SSLVERSION, 3);
-
-        if (strtoupper(substr(@php_uname('s'), 0, 3)) === 'WIN') {
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
         return $curl;
     }
-
-
+    
     /**
      * Create XML value element.
      *
